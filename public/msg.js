1dd5556bac1b3f35b9b08df74b0adb8493c44477
Hamzz12 <ilhamtkj52@gmail.com>
2024-04-05 05:02:13 +0700

    first commit

--- public/msg.js
+++ public/msg.js
@@ -1,3 +1,3 @@
-var MSG_GIFT = "Thank you to |username| for the gift";
-var MSG_WINNER = "Congratulations to |username| for winning";
+var MSG_GIFT = "Terima Kasih |username| Gift nya";
+var MSG_WINNER = "Selamat  |username| kamu telah menang";
 var MSG_TEST = "Test sound";
